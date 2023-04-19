5060ff934aaeebe2d99201c6c51f2c3c18471a6c
zbtrs <1339623011@qq.com>
2023-04-19 14:06:52 +0800

    使得关卡1只输出错误行号，

--- step1/main.cpp
+++ step1/main.cpp
@@ -21,10 +21,14 @@ class MyErrorListener : public BaseErrorListener {
 public:
     void syntaxError(Recognizer *recognizer, Token *offendingSymbol, size_t line, size_t charPositionInLine, const std::string &msg, std::exception_ptr e) override {
         //std::cout << "Error at line " << line << ": " << msg << std::endl;
+        
         if (is_occured.find(line) == is_occured.end()) {
             is_occured[line] = true;
             V.push_back(make_pair(line,msg));
         }
+        
+        //V.push_back(make_pair(line,msg));
+
     }
 };
 
@@ -41,12 +45,17 @@ int main(int argc, const char* argv[]) {
     parser.addErrorListener(&errorListener);
 
     parser.compUnit();
+
+    if (V.empty()) {
+        cout << "no error! please check your g4 file" << endl;
+        return 0;
+    }
+
     sort(V.begin(),V.end());
-    auto line = V.begin();
-    cout << "Error at line " << line->first << endl << line->second << endl;
-
-    //std::cout << tree->toStringTree(&parser) << std::endl << std::endl;
-
+    auto file_name = string(argv[1]);
+    auto slash_pos = file_name.find_last_of("/");
+    file_name = file_name.substr(slash_pos + 1);
+    cout << file_name << ":" << V.begin()->first << endl;
 
     return 0;
 }    
