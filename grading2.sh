1bd33b2e43082645f5dbc72034019b800f77f948
杨茂林 <8575543@qq.com>
2023-04-26 07:56:04 +0800

    Edit by browser

--- grading2.sh
+++ grading2.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
 #ake clean 2>&1 >/dev/null
+cd src
 make 2>&1 >/dev/null
 
-if [ ! -f "scanner" ]; then
+if [ ! -f "parser" ]; then
   echo "无法编译得到分析程序scanner"
 else
   read -p '' case
-  chmod +x scanner
-  ./scanner ./test_cases/$case
-    if [ $(cat ./test_cases/${case}.ast.txt | grep -c "main") -eq 0 ];
-      then
-      echo "AST not correctly dumpped!"
-      cat ./test_cases/${case}.ast.txt
-    else
-      echo "AST file saved in $case.ast.txt:"
-      cat ./test_cases/${case}.ast.txt
-    fi
-
+  chmod +x parser
+  ./parser ./test_cases/$case
 fi
