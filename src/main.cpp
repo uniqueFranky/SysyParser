07739df56a00b116d94d933b5fc469f6f9c5a9a0
杨茂林 <8575543@qq.com>
2023-04-26 07:53:29 +0800

    Edit by browser

--- src/main.cpp
+++ src/main.cpp
@@ -17,21 +17,37 @@ using namespace antlr4;
 using namespace frontend;
 
 int main(int argc, const char* argv[]) {
-    ifstream stream(argv[1]); //读入测试用例
+    bool print_ast = false;
+    std::string filename;
+
+    if (argc > 2) {
+        for (int i = 1; i < argc; i++) {
+            if (argv[i] == std::string("-ast")) {
+                print_ast = true;
+            } else {
+                filename = argv[i];
+            }
+        }
+    }
+
+    ifstream stream(filename); //读入测试用例
     ANTLRInputStream input(stream);  
     SysyLexer lexer(&input);  //词法解析
     CommonTokenStream tokens(&lexer);  //生成TokenStream
     SysyParser parser(&tokens);   //语法解析
     auto root = parser.compUnit();   //得到AST的根节点
+
+    if (!print_ast) return 0;
+
     frontend::AstVisitor visitor;   //自己重写过的AstVisitor类对象
     visitor.visitCompUnit(root);   //visitor模式中从根节点开始遍历
     auto ast = visitor.compileUnit();   
     AstRewriter rewriter;   //可以根据需要对AST rewrite
     rewriter.visit_compile_unit(*ast);
-    string output_file_name = string(argv[1]) + ".ast.txt";
-    ofstream of;
-    of.open(output_file_name);
-    ast->print(of,0);   //打印AST
+    //string output_file_name = string(argv[1]) + ".ast.txt";
+    //ofstream of;
+    //of.open(output_file_name);
+    ast->print(std::cout,0);   //打印AST
 
     return 0;
 }
