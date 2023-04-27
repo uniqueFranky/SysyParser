b172c1e449966ec1399dcd423d16b7fd6e6a29e4
杨茂林 <8575543@qq.com>
2023-04-27 09:57:14 +0800

    Edit by browser

--- src/main.cpp
+++ src/main.cpp
@@ -48,8 +48,8 @@ int main(int argc, const char* argv[]) {
     frontend::AstVisitor visitor;   //自己重写过的AstVisitor类对象
     visitor.visitCompUnit(root);   //visitor模式中从根节点开始遍历
     auto ast = visitor.compileUnit();   
-    AstRewriter rewriter;   //可以根据需要对AST rewrite
-    rewriter.visit_compile_unit(*ast);
+    //AstRewriter rewriter;   //可以根据需要对AST rewrite
+    //rewriter.visit_compile_unit(*ast);
     //string output_file_name = string(argv[1]) + ".ast.txt";
     //ofstream of;
     //of.open(output_file_name);
